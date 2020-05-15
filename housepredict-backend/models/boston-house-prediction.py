fb4edc90a8d79c44d5516ad04a2aa446db374990
Tom Cannon <tom-cannon@playcannon.com>
2020-05-15 16:24:14 -0700

    Backend template created

---
+++ housepredict-backend/models/boston-house-prediction.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_boston
+
+boston = load_boston()
+print(boston)
